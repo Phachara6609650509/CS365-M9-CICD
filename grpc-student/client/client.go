d0aff1054067c9b634e2e998be76816eedec81c3
Phachara Pornpong <phachara.por@dome.tu.ac.th>
2026-03-30 22:52:08 +0700

    Assignments task1,2

---
+++ grpc-student/client/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "grpc-student/studentpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewStudentServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.GetStudent(ctx, &pb.StudentRequest{
+		Id: 101,
+	})
+
+	listRes, err := client.ListStudents(ctx, &pb.Empty{})
+	if err != nil {
+		log.Fatalf("Error calling ListStudents: %v", err)
+	}
+
+	log.Println("Student List:")
+	for _, s := range listRes.Student {
+		log.Printf("ID: %d, Name: %s, Phone: %s", s.Id, s.Name, s.Phone)
+	}
+
+	if err != nil {
+		log.Fatalf("Error calling GetStudent: %v", err)
+	}
+
+	log.Printf("Student Info:")
+	log.Printf("ID: %d", res.Id)
+	log.Printf("Name: %s", res.Name)
+	log.Printf("Major: %s", res.Major)
+	log.Printf("Email: %s", res.Email)
+	log.Printf("Phone: %s", res.Phone)
+}
